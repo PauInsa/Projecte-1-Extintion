cf8a1953501bd257e6a62270b12881249ad4e55c
Pau <pau.insa@enti.cat>
2022-03-22 10:40:49 +0100

    Archivoh

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+
+{
+    public Rigidbody2D rb;
+
+    float horizontal = 0.0f;
+
+    public float speed = 1.0f;
+
+    public float jumpmagnitude;
+
+    public SpriteRenderer spriteRenderer;
+
+    public Transform rayOriginTransform;
+
+    public Animator animator;
+
+    bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            jump();
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            horizontal = -1.0f;
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+            horizontal = 1.0f;
+            spriteRenderer.flipX = true;
+        }
+        else
+            horizontal = 0.0f;
+
+
+
+        grounded = Physics2D.Raycast(rayOriginTransform.position, Vector2.down, 0.01f);
+
+        bool isMoving = horizontal != 0;
+
+        animator.SetBool("isMoving", isMoving);
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+    }
+
+    void jump()
+    {
+        if (grounded == true)
+        {
+
+            rb.AddForce(Vector2.up * jumpmagnitude);
+        }
+    }
+
+}
+
